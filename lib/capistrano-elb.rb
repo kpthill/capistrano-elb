6cc1dd368a25bb8cc1cb51f32d15ddd474cce90d
kpthill <kevin.patrick.thill@gmail.com>
2012-12-05 13:28:59 -0800

    Changed to work better with multiple load balancers.
    Changed the load_from_lb and load_from_tags methods so that they will work
    properly in the case of multiple load balancers in the same region.
    
    Returned to using all load balancers rather than based on tags.  (Currently, you
    cannot remove an instance from two elbs because you can only have one 'elb'
    tag).
    
    Added a print statement to the check config function to show the current load
    balancer configuration.

--- lib/capistrano-elb.rb
+++ lib/capistrano-elb.rb
@@ -23,35 +23,38 @@ class CapELB
     @regions.each do |region|
       @elb.merge!(region => Fog::AWS::ELB.new(ec2credentials.merge(:region=>region)))
     end
-    
-    @lbs = config_from_tags
+
+    @lbs = config_from_lb
   end
-  
+
   def config_from_tags
     lbs = {}
-    @regions.each do |region| 
+    @regions.each do |region|
       @elb[region].load_balancers.each do |lb|
-        lbs.merge!({region => {lb.id => get_instances(region,lb.id)}})
+        lbs[region] = {} unless lbs[region].is_a?(Hash)
+        lbs[region].merge!({lb.id => get_instances(region,lb.id)})
       end
     end
     lbs
   end
-  
+
   def config_from_lb
     lbs = {}
-    @regions.each do |region| 
+    @regions.each do |region|
       @elb[region].load_balancers.each do |lb|
-        lbs.merge!({region => {lb.id => lb.instances}})
+        lbs[region] = {} unless lbs[region].is_a?(Hash)
+        lbs[region].merge!({lb.id => lb.instances})
       end
     end
     lbs
   end
-  
+
   def get_instances(region,lbname)
     @compute[region].tags.select{|tag| tag.key == 'elb' and tag.value==lbname}.map(&:resource_id)
   end
-  
+
   def check_config
+    pp @lbs
     current = config_from_lb
     errors = []
     @lbs.each_pair do |region,lbs|
@@ -62,29 +65,29 @@ class CapELB
         errors << "#{extra} should not be in #{region}/#{lbname}" unless extra.empty?
       end
     end
-    (errors.empty? ? "ELB config correct" : errors) 
+    (errors.empty? ? "ELB config correct" : errors)
   end
-  
+
   def add(serverlist)
     @lbs.each_pair do |region, lbs|
       lbs.each_pair do |lbname, target_instances|
         server_ids = @compute[region].servers.select{|server| serverlist.include? server.dns_name}.map{|server| server.id}
         to_change = server_ids.select{|server_id| target_instances.include? server_id}
         unless to_change.empty?
-          puts "Adding #{to_change} to LB #{lbname} in #{region}" 
-          @elb[region].register_instances_with_load_balancer(to_change, lbname) 
+          puts "Adding #{to_change} to LB #{lbname} in #{region}"
+          @elb[region].register_instances_with_load_balancer(to_change, lbname)
         end
       end
     end
   end
-  
+
   def remove(serverlist)
     @lbs.each_pair do |region, lbs|
       lbs.each_pair do |lbname, target_instances|
         server_ids = @compute[region].servers.select{|server| serverlist.include? server.dns_name}.map{|server| server.id}
         to_change = server_ids.select{|server_id| target_instances.include? server_id}
         unless to_change.empty?
-          puts "Removing #{to_change} from LB #{lbname} in #{region}"        
+          puts "Removing #{to_change} from LB #{lbname} in #{region}"
           @elb[region].deregister_instances_from_load_balancer(to_change, lbname)
         end
       end
